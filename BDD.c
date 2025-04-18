f7a1cf20f9c24cc8f7ba8c45f122e9b4225f7c1d
Makuk369 <makuk369@gmail.com>
2025-04-18 11:01:00 +0200

    first commit

---
+++ BDD.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+// BDD *BDD_create (string bfunkcia, string poradie);
+// BDD *BDD_create_with_best_order (string bfunkcia);
+// char BDD_use (BDD *bdd, string vstupy);
+
+int main(){
+
+
+    return 0;
+}
